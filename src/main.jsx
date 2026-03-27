eccf39c2164c3224fef33916fe85c48d1926487f
lucasito028 <lucastoshihiroito28@gmail.com>
2026-03-27 13:01:38 -0300

    MElhorias

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 createRoot(document.getElementById('root')).render(
+  <GoogleOAuthProvider clientId="43713430205-qvkh4v6e4t86pb4qvp2435t6vjahvffo.apps.googleusercontent.com">
     <StrictMode>
       <App />
     </StrictMode>
+  </GoogleOAuthProvider>
 )
