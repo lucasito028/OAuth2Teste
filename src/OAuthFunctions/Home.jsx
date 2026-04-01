b1bd22e15f33e26e68959e14c323a686daa6051c
lucasito028 <lucastoshihiroito28@gmail.com>
2026-04-01 13:35:57 -0300

    Melhorias

--- src/OAuthFunctions/Home.jsx
+++ src/OAuthFunctions/Home.jsx
@@ -19,6 +19,15 @@ function Home() {
       .replace(/=+$/, '');
   };
 
+  const generateRandomString = (length) => {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return result;
+  };
+
   const microsoftLogin = async () => {
     const codeVerifier = generateRandomString(128);
     const codeChallenge = await sha256(codeVerifier);
